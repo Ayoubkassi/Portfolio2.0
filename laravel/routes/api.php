8b47e075bc2b64d27b8e782598abb89198495073
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-08-25 19:25:35 +0100

    setup new backend

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -17,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register', 'API\RegisterController@register');
+Route::post('login', 'API\RegisterController@login');
+Route::get('test',function(){
+    return 'Hello World';
+});
+// Route::middleware('auth:api')->group( function (){
+//     Route::resource('rooms', 'API\RoomController');
+//     Route::post('join-room','API\RoomController@joinRoom');
+//     Route::get('user-in-room','API\RoomController@userInRoom');
+//     Route::get('leave-room','API\RoomController@leaveRoom');
+//
+//     //for spotify login
+//
+//     Route::get('get-auth-url','API\SpotifyController@getAuth');
+// });
